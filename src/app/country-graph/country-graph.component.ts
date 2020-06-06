f0dc95b76f932301451cffdaa5ebed082cbdde4b
Josh <josh@shabtai.ca>
2020-06-06 18:18:24 -0400

    Fetch and parse data from consolidated file (#28)
    * Fetch and parse data from consolidated file
    
    * New file is pre-processed server-side and should include
      better data for subregions (including recovery data)

--- src/app/country-graph/country-graph.component.ts
+++ src/app/country-graph/country-graph.component.ts
@@ -9,7 +9,7 @@ import { AboutDialogComponent } from '../about-dialog/about-dialog.component';
 import { HelpDialogComponent } from '../help-dialog/help-dialog.component';
 import { DataFetcherService } from '../data-fetcher.service';
 import { PopulationService } from '../population/population.service';
-import { Dataset, GLOBAL_NAME } from '../dataset';
+import { Dataset } from '../dataset';
 import { parse } from '@vanillaes/csv';
 
 // The Chart library (and zoom plugin) are included with the angular 'scripts' config.
@@ -94,27 +94,11 @@ export class CountryGraphComponent implements OnInit {
     }
 
     public getCountries(): Dataset[] {
-        return Object.keys(this.dataFetcher.countries).sort((first, second) => {
-            if (first === GLOBAL_NAME) {
-                return -1;
-            }
-            else if (second === GLOBAL_NAME) {
-                return 1;
-            }
-            else {
-                if (first < second) {
-                    return -1;
-                }
-                else if (second < first) {
-                    return 1;
-                }
-                else {
-                    return 0;
-                }
-            }
-        }).map((country) => {
-            return this.dataFetcher.countries[country];
-        });
+        if (this.dataFetcher.allData == null) {
+            return [];
+        }
+
+        return [this.dataFetcher.allData, ...this.dataFetcher.allData.subsets];
     }
 
     public updateChart() {
@@ -251,7 +235,7 @@ export class CountryGraphComponent implements OnInit {
             'Spain',
             'US',
             'United Kingdom',
-        ].sort().map(name => this.dataFetcher.countries[name]);
+        ].sort().map(name => this.dataFetcher.allData.subsets.find(dataset => dataset.name === name));
 
         this.updateChart();
     }
